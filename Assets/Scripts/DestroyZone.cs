7f8f915d2596a87812a132101a0e4ef37125d9c6
Fststory <fststory@naver.com>
2024-07-11 09:07:02 +0900

    0705 수업

--- Assets/Scripts/DestroyZone.cs
+++ Assets/Scripts/DestroyZone.cs
@@ -10,7 +10,11 @@ public class DestroyZone : MonoBehaviour
         // 부딪힌 대상을 모두 파괴한다. 단, 플레이어는 제외한다.
         if(other.gameObject.tag != "Bullet")
         {
-            Destroy(other.gameObject);
+            // 이름이 Boss가 아닌 게임오브젝트라면...
+            if (other.gameObject.name != "Boss")
+            {
+                Destroy(other.gameObject);
+            }
         }
     }
 }
